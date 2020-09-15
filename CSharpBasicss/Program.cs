b85e9cc1731d9a8788616aa997d0e47618850fc6
Anusha Tandukar <anushatandukar67@gmail.com>
2020-09-15 12:46:58 +0545

    submitted

---
+++ CSharpBasicss/Program.cs
@@ -0,0 +1,10 @@
+using LearnCollections;
+public class Program
+{
+    static void Main()
+    {
+        Collections collection = new Collections();
+        collection.LearnDictionary();
+
+    }
+}
