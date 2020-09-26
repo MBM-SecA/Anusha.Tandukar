1e84a3c1c409cabdd7a580e830ea559e47cee8e9
Anusha Tandukar <anushatandukar67@gmail.com>
2020-09-26 21:29:43 +0545

    Added countries

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,39 +1,34 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-using System;
-
+ 
 namespace LINQ
 {
     class Program
     {
         static void Main()
         {
-            // LINQ - Language Integrated Query
-
-            int[] numbers = { 23,34, 56, 35, 67, 10,71 };
-
-            // Fetch numbers which are gretaer than 50 and less than 70
-
-            // Using method syntax - Lambda expression
-            var result1 = numbers.Where(x => x>50 && x < 70);
-
-            //Using sql query syntax - query expression
-            var result2 = from x in numbers where x >50 && x < 70 select x;
-
-            string[] names = {"anusha", "ram", "sita",  "hari", "rita", "rhea", "Rosy"};
-
-            // Names having length > 3 and starts with letter 'R'
-
-            var result3 = names.Where(y => y.Length > 3 && y.StartsWith("r"));
-            var result4 = from y in names where y.Length > 3 && y.ToUpper().StartsWith("R") select y;
-            foreach(var name in result4)
+            //LINQ - Language Integrated Query
+            //LINQ to objects
+            int[] numbers = { 23, 34, 56, 12, 23, 89, 67, 10, 445, 32, 66, 69 };
+            //fetch numbers which are greater than 50
+            var result1 = numbers.Where(x => x > 50 && x < 70);
+            //Using Sql query syntax - query expression
+            var result2 = from x in numbers where x > 50 && x < 70 select x;
+ 
+            string[] names = { "Anusha", "ram", "Ria", "shyam", "Ruby", "Rajesh"};
+            var result3 = names.Where(y => y.Length > 3 && y.StartsWith("R"));
+            var result4 = from y in names where y.Length > 3 && y.ToLower().StartsWith("r") select y;
+ 
+            var countries = Country.GetCountries();
+            var EuCountries = from country in countries where country.Continent == "Europe" && country.Population  < 100000 select country.Name;
+            foreach(var country in EuCountries)
             {
-                Console.WriteLine(name);
+                Console.WriteLine(country);
             }
-            
-            
-           var countries = Country.GetCountries();
-           var asianCountries = from country in countries where country.Continent == "Asia" select country;
+ 
+//List countries in Europe which has population less than 100k.
+//List countries in Asia which are not ever invaded.
         }
     }
 }
